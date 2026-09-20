72e268e64d12e29275126cb2131a2f202bee7064
SaraLimaC <ayelen6666@gmail.com>
2026-09-20 14:31:27 +0200

    funcion colores

--- m1.js
+++ m1.js
@@ -1,1 +1,22 @@
-documet
+
+let texto = document.getElementById("texto");
+document.getElementsByClassName("red").addEventListener("click", rojo() );
+
+function rojo(){
+    texto.style.color = "red";
+
+}
+
+document.getElementsByClassName("blue").addEventListener("click", blue() );
+
+function blue(){
+    texto.style.color = "blue";
+
+}
+
+document.getElementsByClassName("green").addEventListener("click", green() );
+
+function green(){
+    texto.style.color = "green";
+
+}
